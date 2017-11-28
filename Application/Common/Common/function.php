91e40bed8a737527f8c741cf854f78509bdc1d22
Rabin <zhangkunch@qq.com>
2017-11-28 22:11:11 +0800

    添加骰子

--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -41,45 +41,48 @@ function get_newbtc(){
 // 获得当前的url
 
 
-function get_current_url(){
-
-
+function get_current_url()
+{
 	$url = "http://" . $_SERVER['SERVER_NAME'];
-
-
 	$url .= $_SERVER['REQUEST_URI'];
-
-
 	return $url;
-
-
 }
-function get_btc(){
+
+function get_btc()
+{
      $url = 'https://www.okcoin.cn/api/v1/ticker.do?symbol=btc_cny';
      $info  = send_get($url);
      $list = json_decode($info,true);
      return $list;
 }
-function get_btc_list(){
+
+function get_btc_list()
+{
 	 $url = 'https://www.okcoin.cn/api/v1/trades.do?symbol=btc_cny&since=600';
      $info  = send_get($url);
      $list = json_decode($info,true);
      return $list;
 }
-function get_btc_new(){
+
+function get_btc_new()
+{
 	 $url = 'https://www.okcoin.cn/api/v1/kline.do?symbol=btc_cny&type=1min';
      $info  = send_get($url);
      $list = json_decode($info,true);
      return $list;
 }
-function send_get($url){
+
+function send_get($url)
+{
 	$ch = curl_init($url) ;  
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true) ; // 获取数据返回  
     curl_setopt($ch, CURLOPT_BINARYTRANSFER, true) ; // 在启用 
     $output = curl_exec($ch) ; 
     return $output;
 }
-function send_post($url,$data=null){
+
+function send_post($url,$data=null)
+{
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
@@ -100,583 +103,219 @@ function microtime_float()
    list($usec, $sec) = explode(" ", microtime());
    return ((float)$usec + (float)$sec);
 }
+
 //返回当前的毫秒时间戳
 function msectime() {
        list($tmp1, $tmp2) = explode(' ', microtime());
        return (float)sprintf('%.0f', (floatval($tmp1) + floatval($tmp2)) * 1000);
 }
- 
 
 /** 格式化时间戳，精确到毫秒，x代表毫秒 */
-
 function microtime_format($format = 'x', $utimestamp = null)
 {
     if (is_null($utimestamp))
            $utimestamp = microtime(true);
- 
        $timestamp = floor($utimestamp);
        $milliseconds = round(($utimestamp - $timestamp) * 1000000);
- 
        return date(preg_replace('`(?<!\\\\)x`', $milliseconds, $format), $timestamp);
 }
+
 //发送微信消息
-
-
-function sendwen($openid,$msg){
-
-
+function sendwen($openid,$msg)
+{
     $token = wx_token();
-
-
     $url = "https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token={$token}";
-
-
             $msg = str_replace('"', '\\"', $msg);
-
-
             $data = '{"touser":"' . $openid . '","msgtype":"text","text":{"content":"' . $msg . '"}}';
-
-
     $ch = curl_init();
-
-
     curl_setopt($ch, CURLOPT_URL, $url);
-
-
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-
-
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-
-
     curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-
-
     curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (compatible; MSIE 5.01; Windows NT 5.0)');
-
-
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-
-
     curl_setopt($ch, CURLOPT_AUTOREFERER, 1);
-
-
     curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-
-
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-
     $info = curl_exec($ch);
-
-
     curl_close($ch);
-
-
     return $info;
-
-
 }
 
-
-function wxuser($openid){
-
-
-  $token = wx_token();  
-
-
+function wxuser($openid)
+{
+  $token = wx_token();
   $info = file_get_contents('https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$token.'&openid='.$openid.'&lang=zh_CN');
-
-
   return json_decode($info,true);
-
-
 }
 
 
 //获取token
-
-
-function wx_token(){
-
-
-    $appid = $GLOBALS['_CFG']['mp']['appid']; 
-
-
-    $appsecret = $GLOBALS['_CFG']['mp']['appsecret']; 
-
-
+function wx_token()
+{
+    $appid = $GLOBALS['_CFG']['mp']['appid'];
+    $appsecret = $GLOBALS['_CFG']['mp']['appsecret'];
     $token_url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.$appid.'&secret='.$appsecret;
-
-
     $token_content = file_get_contents($token_url);
-
-
     $token = @json_decode($token_content, true);
-
-
     $token = $token['access_token'];
-
-
     return $token;
-
-
 }
-
 
 // 补全url
-
-
-function complete_url($url){
-
-
+function complete_url($url)
+{
 	$host = $GLOBALS['_CFG']['site']['url'];
-
-
 	if(substr($url,0,1) == '.'){
-
-
 		return $host.__ROOT__.substr($url,1);
-
-
-	}
-
-
-	elseif(substr($url,0,7) != 'http://' && substr($url,0,8) != 'https://'){
-
-
+	} elseif (substr($url,0,7) != 'http://' && substr($url,0,8) != 'https://'){
 		return $host.$url;
-
-
-	}
-
-
-	else{
-
-
+	} else {
 		return $url;
-
-
 	}
-
-
 }
-
-
-
-
-
-
-
 
 // 根据GET参数生成一个支付URL签名
-
-
-function make_url_sign($get){
-
-
+function make_url_sign($get)
+{
 	unset($get['sign']);
-
-
 	ksort($get, SORT_STRING);
-
-
 	$string =  urldecode(http_build_query($get));
-
-
 	$string = xmd5($string);
-
-
 	return substr($string,16,16);
-
-
 }
-
-
-
-
 
 // 根据GET参数检查URL签名
-
-
-function check_url_sign($get){
-
-
+function check_url_sign($get)
+{
 	$sign = $get['sign'];
-
-
 	if(empty($sign))return false;
-
-
 	unset($get['sign']);
-
-
 	$new_sign = make_url_sign($get);
-
-
 	return $new_sign == $sign ? true : false;
-
-
 }
-
-
-
-
-
-
-
 
 // 根据自定义菜单类型返回名称
-
-
-function get_selfmenu_type($type){
-
-
+function get_selfmenu_type($type)
+{
 	$type_name = '';
-
-
 	switch($type){
-
-
 		case 'click':
-
-
 			$type_name = '点击推事件';
-
-
 			break;
-
-
 		case 'view':
-
-
 			$type_name = '跳转URL';
-
-
 			break;
-
-
 		case 'scancode_push':
-
-
 			$type_name = '扫码推事件';
-
-
 			break;
-
-
 		case 'scancode_waitmsg':
-
-
 			$type_name = '扫码推事件且弹出“消息接收中”提示框';
-
-
 			break;
-
-
 		case 'pic_sysphoto':
-
-
 			$type_name = '弹出系统拍照发图';
-
-
 			break;
-
-
 		case 'pic_photo_or_album':
-
-
 			$type_name = '弹出拍照或者相册发图';
-
-
 			break;
-
-
 		case 'pic_weixin':
-
-
 			$type_name = '弹出微信相册发图器';
-
-
 			break;
-
-
 		case 'location_select':
-
-
 			$type_name = '弹出地理位置选择器';
-
-
 			break;
-
-
 		default : $type_name = '不支持的类型';
-
-
 	}
-
-
 	return $type_name;
-
-
 }
-
-
-
-
 
 // 获取列表模板列表
-
-
-function get_list_tpl(){
-
-
+function get_list_tpl()
+{
 	$tpl_path = C('list_tpl_path');
-
-
 	$dir_list = scandir($tpl_path);
-
-
 	foreach($dir_list as $k => $v){
-
-
 		if(in_array($v,array('.','..'))  || !is_dir($tpl_path.'/'.$v)){
-
-
 			unset($dir_list[$k]);
-
-
 		}
-
-
 	}
-
-
 	return $dir_list;
-
-
 }
-
-
-
-
 
 // 返回订单的状态
-
-
 function get_order_status($status){
-
-
 	$arr = array(
-
-
 		-1 => '已取消',
-
-
 		0 => '待审核',
-
-
 		1 => '待服务',
-
-
 		2 => '已完成'
-
-
 	);
-
-
 	if(!empty($arr[$status]))return $arr[$status];
-
-
 	else return '未知';
-
-
 }
-
-
-
-
 
 // 发送短信
-
-
 function send_sms($mobile,$msg,$sign = null, $orig = false){
-
-
 	if(empty($sign))$sign = C('SMS_SIGN');
-
-
 	$msg = "【".$sign."】".$msg;
-
-
 	$url = "http://api.smsbao.com/sms?u=".C('SMS_USER')."&p=".md5(C('SMS_PASS'))."&m={$mobile}&c=".urlencode($msg);
-
-
 	$rt =  file_get_contents($url);
-
-
 	M('sms_log') -> add(array(
-
-
 		'mobile' => $mobile,
-
-
 		'msg' => $msg,
-
-
 		'result' => $rt,
-
-
 		'create_time' => NOW_TIME
-
-
 	));
-
-
 	if($orig)return $rt;
-
-
 	return $rt == '0' ? true : false;
-
-
 }
-
-
-
-
 
 // 返回短信状态
-
-
-function get_sms_result($rt){
-
-
+function get_sms_result($rt)
+{
 	$code = array(
-
-
 		0  => '发送成功',
-
-
 		30 => '密码错误',
-
-
 		40 => '账号不存在',
-
-
 		41 => '余额不足',
-
-
 		42 => '帐号过期',
-
-
 		43 => 'IP地址限制',
-
-
 		50 => '内容含有敏感词',
-
-
 		51 => '手机号码不正确'
-
-
 	);
-
-
 	$return = !empty($code[$rt]) ? $code[$rt] : '其他错误';
-
-
 	return  $return;
-
-
 }
-
-
-
-
 
 // 记录错误日志
-
-
-function elog($msg){
-
-
+function elog($msg)
+{
 	$log_file = "./log/error/".date('Ym/d').".log";
-
-
 	if(!is_dir(dirname($log_file))){
-
-
 		mkdir(dirname($log_file),0777,1);
-
-
 	}
-
-
 	$log_arr = array(
-
-
 		date('H:i:s'),
-
-
 		$msg,
-
-
 		get_current_url(),
-
-
 		$_SERVER['HTTP_USER_AGENT']
-
-
 	);
-
-
 	file_put_contents($log_file,implode("\t",$log_arr)."\n",FILE_APPEND);
-
-
 }
 
-
-
-
-
-
-
-
 // 发送客服提示消息
-
-
-function send_msg($openid,$str){
-
-
+function send_msg($openid,$str)
+{
 	$dd = new \Common\Util\ddwechat($GLOBALS['_CFG']['mp']);
-
-
 	$accesstoken = $dd -> getaccesstoken();
-
-
 	$msg = array(
-
-
 		'touser' => $openid,
-
-
 		'msgtype' => 'text',
-
-
 		'text' => array(
-
-
 			'content' => $str
-
-
 		)
-
-
 	);
-
-
 	$dd -> custommsg($msg);
-
-
 }
 
 
@@ -2024,349 +1663,123 @@ function reward_streight($tree_node,$level){
 
 
 			'zhitui' => array('exp','zhitui+1')
-
-
 		));
-
-
 		if($money)flog($parent_user_id,'money',$money,31,$remark);
-
-
 		if($tiliu)flog($parent_user_id,'points',$tiliu,31,$remark);
-
-
 	}
-
-
 }
-
-
-
-
 
 //发放普通懒人奖，level表示懒人奖的层数
-
-
 function reward_lazy($node_info){
-
-
 	// 懒人奖金额
-
-
 	$reward = $GLOBALS['_CFG']['paiwei']['lazy_money'];
-
-
 	// 奖励层数
-
-
 	$levels = $GLOBALS['_CFG']['paiwei']['lazy_level'];
-
-
-	
-
-
 	// 获得上面levels父级的节点pos
-
-
 	$parent_pos_arr = get_parent_nodes($node_info['x'],$node_info['y'],$levels);
-
-
 	if(!empty($parent_pos_arr)){
-
-
 		// 对level层以内的上级发放懒人奖
-
-
 		$where = array(
-
-
 			'pos' => array('in',$parent_pos_arr)
-
-
 		);
-
-
-		
-
-
 		$parent_nodes = M('tree') -> where($where) -> field() -> select();
-
-
-		
-
-
 		$user_id_arr = array();
-
-
 		foreach($parent_nodes as $parent_node){
-
-
 			$user_id_arr[] = $parent_node['user_id'];
-
-
 		}
-
-
-		
-
 
 		// 发放懒人奖到用户
-
-
 		// M('user') -> where(array('id' => array('in',$user_id_arr))) -> save(array(
-
-
 		// 	'money' => array('exp','money+'.$reward),
-
-
 		// 	'reward_lazy' => array('exp','reward_lazy+'.$reward),
-
-
 		// ));
 
-
-		
-
-
 		// 累加对应的奖励次数
-
-
 		M('tree') -> where(array(
-
-
 			'pos' => array('in',$parent_pos_arr)
-
-
 		)) -> setInc('lazy_times');
 
-
-		
-
-
 		// 循环增加懒人奖财务记录
-
-
 		//$i = 0;
-
-
 		foreach($parent_nodes as $parent_node){
-
-
 			$times = $parent_node['times'] +1;
-
-
 			$remark = $GLOBALS['_CFG']['level'][$parent_node['level']]['name'].$times;
-
-
 			if($reward)flog($parent_node['user_id'],'money',$reward,10,$remark);
-
-
-			
-
-
 			$parent_info = M('user') -> find($parent_node['user_id']);
-
-
 			M('user') -> where(array('id' => $parent_node['user_id'])) -> save(array(
-
-
 			'money' => array('exp','money+'.$reward),
-
-
 			'reward_lazy' => array('exp','reward_lazy+'.$reward),
-
-
 		    ));
-
-
 			$msg = "恭喜您获得了一个懒人奖：\n金额：{$reward}\n时间：".date('Y-m-d H:i:s');
-
-
 			sendwen($parent_info['openid'],$msg);
-
-
 		}
-
-
 	}
-
-
 	return ;
-
-
 }
-
-
-
-
 
 // 根据我的位置找我的下级的位置
-
-
-function find_child_pos($x,$y){
-
-
+function find_child_pos($x,$y)
+{
 	$x ++;
-
-
 	if($y==0){
-
-
 		$y = 0;
-
-
-	}
-
-
-	else{
-
-
+	} else{
 		$y = 2*$y;
-
-
 	}
-
-
 	return array('x' => $x,'y' => $y);
-
-
 }
 
-
-
-
-
-// 根据位置 （xy）获得序号 
-
-
-function get_index_by_pos($x,$y){
-
-
+// 根据位置 （xy）获得序号
+function get_index_by_pos($x,$y)
+{
 	return pow(2,$x)-1+$y;
-
-
 }
-
-
-
-
 
 // 根据位置获得父节点的位置
-
-
-function get_parent_pos($x,$y){
-
-
+function get_parent_pos($x,$y)
+{
 	if($x<=0){
-
-
 		return false;
-
-
 	}
-
-
-	
-
-
 	$x = $x-1;
-
-
 	$y = floor(($y)/2);
-
-
 	return array('x' => $x,'y' => $y);
-
-
 }
-
-
-
-
 
 // 获得当前时间戳
-
-
-function get_now(){
-
-
+function get_now()
+{
 	if(CLI === true)return time();
-
-
 	else return NOW_TIME;
-
-
 }
-
-
-
-
 
 // 转为微信头像地址大小,默认原始图片(/0)，其他支持132*132(/132)、96*96(/96)、64*64(/64)、46*46(/46)
-
-
 // 头像原图可能很大，下载和访问都很慢，根据需要制定大小可以加快访问速度
-
-
-function headsize($url,$size = 0){
-
-
+function headsize($url,$size = 0)
+{
 	$arr = explode('/',$url);
-
-
 	if(!is_numeric($arr[count($arr)-1]))return $url;
-
-
 	$arr[count($arr)-1] = $size;
-
-
 	return implode('/',$arr);
-
-
 }
 
-
-
-
-
 // 取得支付方式
-
-
 function get_payway($payway){
-
-
 	$arr = array(
-
-
 		'money' => '余额',
-
-
 		'points' => '积分',
-
-
 		'wxpay' => '微信',
-
-
 		'alipay' => '支付宝'
-
-
 	);
-
-
 	return $arr[$payway];
-
-
 }
 
 
 function guid()
 {
-    if (function_exists('com_create_guid'))
-    {
+    if (function_exists('com_create_guid')) {
         return com_create_guid();
-    }
-    else
-    {
+    } else {
         mt_srand((double)microtime()*10000);//optional for php 4.2.0 and up.
         $charid = strtoupper(md5(uniqid(rand(), true)));
         $hyphen = chr(45);// "-"
@@ -2381,7 +1794,8 @@ function guid()
     }
 }
 
-function random($length, $numeric = FALSE) {
+function random($length, $numeric = FALSE)
+{
     $seed = base_convert(md5(microtime().$_SERVER['DOCUMENT_ROOT']), 16, $numeric ? 10 : 35);
     $seed = $numeric ? (str_replace('0', '', $seed).'012340567890') : ($seed.'zZ'.strtoupper($seed));
     if($numeric) {
